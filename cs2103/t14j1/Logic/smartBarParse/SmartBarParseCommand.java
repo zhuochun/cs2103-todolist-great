2d16ff7e7ffbcf095c07cdd2a00e4874a6985df0
Song Yangyu <Song Yangyu@SongYangyu-PC.nus.edu.sg>
2011-09-11 15:27:35 +0800

    commit to merge with Shubhum's update

--- cs2103/t14j1/Logic/smartBarParse/SmartBarParseCommand.java
+++ cs2103/t14j1/Logic/smartBarParse/SmartBarParseCommand.java
@@ -1,102 +1,80 @@
 package cs2103.t14j1.Logic.smartBarParse;
 
-import java.util.Calendar;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Define the basic property of the task
- * for the int type, -1 means doesn't exist;
- * for the object type, null means doesn't exist 
- * 
+ * this class would parse the command from SmartBar.
+ *  
+ *  To use it, you would create an intense of it using its constructor, with the
+ *   passed command (String) as a parameter.
+ *  
+ *  Example:
+ *  
+ *  @code
+ *  // smartBarStr is the String passed from smart bar GUI
+ *  SmartBarParseCommand newCommand = new SmartBarParseCommand(smartBarStr); 
+ *	 
+ *	// to get the command type
+ *	newCommend
+ * 	@endcode
+ * 	
  * @author Song Yangyu
- * 
  */
-class BasicTask{
-
-	
-	String title;
-	
-	String date;
-	
-	String place;
-	
-	/**
-	 * Unit: minutes
-	 */
-	int duration;
-	
-	/**
-	 * Initially to be 1~3
-	 */
-	int priority;
-	
-	/**
-	 * Assume to have one initially
-	 */
-	String tag;
-	
-	boolean completed;
-	
-	BasicTask(String taskStr){
-		title = taskStr;
-		
-		
-		
-		// calendar
-		
-	}
-	
-	@Override
-	public String toString(){
-		String res = "Title: " + this.title;
-		
-		return res;
-	}
-}
-
 public class SmartBarParseCommand {
 
-	// @group: regular expressions for match
+	// @group regex: regular expressions for match
 	
 	// always match am/pm first, then match the 24h time point
-	static final String regTimePointAmPm =
+	private static final String regTimePointAmPm =
 			"(0?[1-9]|1[012])(:\\ [0-5]\\d){0,2}(\\ [AP]M)";
-	static final String regTimePoint24H = 
+	private static final String regTimePoint24H = 
 			"([01]?\\d|2[0-3])(:[0-5]\\d){0,2}";
-	static final String regTimePointInteger =
+	private static final String regTimePointInteger =
 			"(1[012]|[1-9])([AP]M)";
-	static final String regTimeFormat = 
+	private static final String regTimeFormat = 
 			"(" + regTimePointAmPm + "|" + regTimePoint24H + ")";
 	
-	static final String regYear =
+	private static final String regYear =
 			"(19|20)\\d\\d)";
-	static final String reg =
+	private static final String reg =
 			"(19|20)\\d\\d)";
 	
-	static final String regMonth =
+	private static final String regMonth =
 			"(Jan|January|Feb|)";
-	static final String regDateFormatDM = 
+	private static final String regDateFormatDM = 
 			"(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])([- /.](19|20)\\d\\d)?";
-	static final String regDateFormatMD = 
+	private static final String regDateFormatMD = 
 			"(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])([- /.](19|20)\\d\\d)?";
-	static final String regMatchCalendar = "at";
-	// @endgroup
+	private static final String regMatchCalendar = "at";
+	// @endgroup regex
 	
+	
+	// @group regex-match
+	
+	// @endgroup regex-match
 	
 	// filed of this class
 	String command;
 	
 	/** The constructor
-	 * @param command - 
+	 * @param command - the command passed from the smart bar GUI
 	 */
 	public SmartBarParseCommand(String command) {
 		this.command = command;
 	}
 	
+	/**
+	 * @return the start date of the task; timestamp of the mid-night of that date
+	 */
+	public Integer getStartDate(){
+		return 0;
+	}
 	
-	/** Get the start time of the task
+	/** Get the start time of the task; the number of seconds relevative to the
+	 *  start of the day.
+	 *  Example, if I 
 	 * @return
 	 */
 	public Integer getStartTime(){
@@ -104,6 +82,13 @@ public class SmartBarParseCommand {
 		return 1;
 	}
 	
+	public Integer getEndTime(){
+		return 1;
+	}
+	
+	public Integer getDuration(){
+		return 1;
+	}
 	
 	/**
 	 * @param args
@@ -113,7 +98,7 @@ public class SmartBarParseCommand {
 		
 		// test match here
 		String taskStr = "at 10:00 am ~ 12:00 pm";	// test time
-		Pattern timePoint = Pattern.compile(regTimeFormat);
+		Pattern timePoint = Pattern.compile(regTimePointAmPm);
 		Matcher timePointMatcher = timePoint.matcher(taskStr);
 		while(timePointMatcher.find()){
 			System.out.println(timePointMatcher.group());

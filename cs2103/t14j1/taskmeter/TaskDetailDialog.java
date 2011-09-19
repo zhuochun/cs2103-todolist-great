1c60a45c4668e7a1f1ef3ece0ce93c46192cf1ae
Shubham Goyal <shubham.goyal@live.in>
2011-09-19 22:33:40 +0800

    Merge

---
+++ cs2103/t14j1/taskmeter/TaskDetailDialog.java
@@ -0,0 +1,50 @@
+package cs2103.t14j1.taskmeter;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class TaskDetailDialog extends Dialog {
+
+	protected Object result;
+	protected Shell shell;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public TaskDetailDialog(Shell parent) {
+		super(parent, SWT.NONE);
+		setText("SWT Dialog");
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shell.open();
+		shell.layout();
+		Display display = getParent().getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shell = new Shell(getParent(), getStyle());
+		shell.setSize(450, 300);
+		shell.setText(getText());
+
+	}
+
+}
--- cs2103/t14j1/taskmeter/TaskMeter.java
+++ cs2103/t14j1/taskmeter/TaskMeter.java
@@ -4,6 +4,10 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
@@ -11,12 +15,20 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 
+/**
+ * TaskMeter Main Graphic User Interface
+ * 
+ * @author Zhuochun
+ *
+ */
 public class TaskMeter extends Shell {
 	
 	private static ResourceBundle resourceBundle = ResourceBundle.getBundle("taskmeter_res");
@@ -24,6 +36,7 @@ public class TaskMeter extends Shell {
 	private List taskLists;
 	private Table taskTable;
 	private Text smartBar;
+	private boolean isModified;
 	
 	/**
 	 * Launch the application.
@@ -54,101 +67,92 @@ public class TaskMeter extends Shell {
 		setMinimumSize(new Point(750, 500));
 		setLayout(null);
 		
-		Menu menu = new Menu(this, SWT.BAR);
-		setMenuBar(menu);
+		createMenuBar();
+		createSmartBar();
+		createTaskList();
+		createTaskTable();
+		createFilterButtons();
+		createStatusBar();
 		
-		MenuItem mntmUser = new MenuItem(menu, SWT.CASCADE);
-		mntmUser.setText(getResourceString("menu.user"));
+		createContents();
+	}
+
+	/**
+	 * Create contents of the shell.
+	 */
+	protected void createContents() {
+		addShellListener(new ShellAdapter() {
+			public void shellClosed(ShellEvent e) {
+				e.doit = closeTaskMeter();
+			}
+		});
 		
-		Menu menuUser = new Menu(mntmUser);
-		mntmUser.setMenu(menuUser);
+		setText(getResourceString("app.title.full"));
+		setSize(750, 500);
 		
-		MenuItem mntmSave = new MenuItem(menuUser, SWT.NONE);
-		mntmSave.setText(getResourceString("save"));
-		
-		new MenuItem(menuUser, SWT.SEPARATOR);
-		
-		MenuItem mntmExit = new MenuItem(menuUser, SWT.NONE);
-		mntmExit.setText(getResourceString("exit"));
-		
-		MenuItem mntmEdit = new MenuItem(menu, SWT.CASCADE);
-		mntmEdit.setText(getResourceString("menu.edit"));
-		
-		Menu menuEdit = new Menu(mntmEdit);
-		mntmEdit.setMenu(menuEdit);
-		
-		MenuItem mntmUndo = new MenuItem(menuEdit, SWT.NONE);
-		mntmUndo.setText(getResourceString("undo"));
-		
-		MenuItem mntmRedo = new MenuItem(menuEdit, SWT.NONE);
-		mntmRedo.setText(getResourceString("redo"));
-		
-		new MenuItem(menuEdit, SWT.SEPARATOR);
-		
-		MenuItem mntmSearch = new MenuItem(menuEdit, SWT.NONE);
-		mntmSearch.setText(getResourceString("find"));
-		
-		MenuItem mntmWindow = new MenuItem(menu, SWT.CASCADE);
-		mntmWindow.setText(getResourceString("menu.window"));
-		
-		Menu menuWindow = new Menu(mntmWindow);
-		mntmWindow.setMenu(menuWindow);
-		
-		MenuItem mntmListView = new MenuItem(menuWindow, SWT.CHECK);
-		mntmListView.setSelection(true);
-		mntmListView.setText(getResourceString("list"));
-		
-		MenuItem mntmCalendarView = new MenuItem(menuWindow, SWT.CHECK);
-		mntmCalendarView.setText(getResourceString("calendar"));
-		
-		MenuItem mntmSettings = new MenuItem(menu, SWT.CASCADE);
-		mntmSettings.setText(getResourceString("menu.setting"));
-		
-		Menu menuSetting = new Menu(mntmSettings);
-		mntmSettings.setMenu(menuSetting);
-		
-		MenuItem mntmUserSettings = new MenuItem(menuSetting, SWT.NONE);
-		mntmUserSettings.setText(getResourceString("setting"));
-		
-		MenuItem mntmHelps = new MenuItem(menu, SWT.CASCADE);
-		mntmHelps.setText(getResourceString("menu.help"));
-		
-		Menu menuHelp = new Menu(mntmHelps);
-		mntmHelps.setMenu(menuHelp);
-		
-		MenuItem mntmStatistics = new MenuItem(menuHelp, SWT.NONE);
-		mntmStatistics.setText(getResourceString("statistics"));
-		
-		new MenuItem(menuHelp, SWT.SEPARATOR);
-		
-		MenuItem mntmHelp = new MenuItem(menuHelp, SWT.NONE);
-		mntmHelp.setText(getResourceString("help"));
-		
-		new MenuItem(menuHelp, SWT.SEPARATOR);
-		
-		MenuItem mntmAbout = new MenuItem(menuHelp, SWT.NONE);
-		mntmAbout.setText(getResourceString("about"));
-		
+		isModified = false;
+	}
+
+	/**
+	 * 
+	 */
+	private void createSmartBar() {
 		smartBar = new Text(this, SWT.BORDER);
 		smartBar.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
 		smartBar.setBounds(5, 5, 735, 30);
+	}
+
+	/**
+	 * Creates the menu at the top of the shell where most
+	 * of the programs functionality is accessed.
+	 *
+	 * @return	The <code>Menu</code> widget that was created
+	 */
+	private Menu createMenuBar() {
+		Menu menuBar = new Menu(this, SWT.BAR);
+		setMenuBar(menuBar);
 		
-		Label statusBarSeperator = new Label(this, SWT.SEPARATOR | SWT.HORIZONTAL);
-		statusBarSeperator.setBounds(0, 425, 750, 2);
+		createUserMenu(menuBar);
+		createEditMenu(menuBar);
+		createWindowMenu(menuBar);
+		createSettingMenu(menuBar);
+		createHelpMenu(menuBar);
 		
-		statusBar = new Label(this, SWT.NONE);
-		statusBar.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
-		statusBar.setBounds(10, 430, 730, 20);
-		statusBar.setText(getResourceString("msg.welcome"));
-		
+		return menuBar;
+	}
+
+	/**
+	 * 
+	 */
+	private void createTaskList() {
 		taskLists = new List(this, SWT.BORDER);
 		taskLists.setBounds(5, 40, 150, 350);
 		
 		Button btnAddANew = new Button(this, SWT.NONE);
+		btnAddANew.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+			}
+		});
 		btnAddANew.setBounds(5, 395, 150, 25);
 		btnAddANew.setText(getResourceString("add.list"));
-		
+	}
+
+	/**
+	 * 
+	 */
+	private void createTaskTable() {
 		taskTable = new Table(this, SWT.BORDER | SWT.FULL_SELECTION);
+		taskTable.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				TableItem[] items = taskTable.getSelection();
+				if (items.length > 0) {
+					editTask(items[0]);
+				}
+			}
+		});
 		taskTable.setBounds(160, 40, 579, 350);
 		taskTable.setHeaderVisible(true);
 		taskTable.setLinesVisible(true);
@@ -182,7 +186,12 @@ public class TaskMeter extends Shell {
 		tblclmnCompleted.setMoveable(true);
 		tblclmnCompleted.setWidth(75);
 		tblclmnCompleted.setText(getResourceString("table.completed"));
-		
+	}
+
+	/**
+	 * 
+	 */
+	private void createFilterButtons() {
 		Button btnAll = new Button(this, SWT.NONE);
 		btnAll.setBounds(160, 395, 35, 25);
 		btnAll.setText(getResourceString("filter.all"));
@@ -214,18 +223,190 @@ public class TaskMeter extends Shell {
 		Button btnWithoutDate = new Button(this, SWT.NONE);
 		btnWithoutDate.setBounds(650, 395, 90, 25);
 		btnWithoutDate.setText(getResourceString("filter.nodate"));
-		
-		createContents();
 	}
 
 	/**
-	 * Create contents of the shell.
+	 * 
 	 */
-	protected void createContents() {
-		setText(getResourceString("app.title.ful"));
-		setSize(750, 500);
+	private void createStatusBar() {
+		Label statusBarSeperator = new Label(this, SWT.SEPARATOR | SWT.HORIZONTAL);
+		statusBarSeperator.setBounds(0, 425, 750, 2);
+		
+		statusBar = new Label(this, SWT.NONE);
+		statusBar.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+		statusBar.setBounds(10, 430, 730, 20);
+		
+		setStatusBar(getResourceString("msg.welcome"));
 	}
 
+	/**
+	 * @param menuBar
+	 */
+	private void createHelpMenu(Menu menuBar) {
+		MenuItem mntmHelps = new MenuItem(menuBar, SWT.CASCADE);
+		mntmHelps.setText(getResourceString("menu.help"));
+		
+		Menu menuHelp = new Menu(mntmHelps);
+		mntmHelps.setMenu(menuHelp);
+		
+		MenuItem mntmStatistics = new MenuItem(menuHelp, SWT.NONE);
+		mntmStatistics.setText(getResourceString("statistics"));
+		
+		new MenuItem(menuHelp, SWT.SEPARATOR);
+		
+		MenuItem mntmHelp = new MenuItem(menuHelp, SWT.NONE);
+		mntmHelp.setText(getResourceString("help"));
+		
+		new MenuItem(menuHelp, SWT.SEPARATOR);
+		
+		MenuItem mntmAbout = new MenuItem(menuHelp, SWT.NONE);
+		mntmAbout.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				MessageBox box = new MessageBox(getShell(), SWT.NONE);
+				box.setText(getResourceString("msg.about"));
+				box.setMessage("Current Version: " + getResourceString("app.version"));
+				box.open();
+			}
+		});
+		mntmAbout.setText(getResourceString("about"));
+	}
+
+	/**
+	 * @param menuBar
+	 */
+	private void createSettingMenu(Menu menuBar) {
+		MenuItem mntmSettings = new MenuItem(menuBar, SWT.CASCADE);
+		mntmSettings.setText(getResourceString("menu.setting"));
+		
+		Menu menuSetting = new Menu(mntmSettings);
+		mntmSettings.setMenu(menuSetting);
+		
+		MenuItem mntmUserSettings = new MenuItem(menuSetting, SWT.NONE);
+		mntmUserSettings.setText(getResourceString("setting"));
+	}
+
+	/**
+	 * @param menuBar
+	 */
+	private void createWindowMenu(Menu menuBar) {
+		MenuItem mntmWindow = new MenuItem(menuBar, SWT.CASCADE);
+		mntmWindow.setText(getResourceString("menu.window"));
+		
+		Menu menuWindow = new Menu(mntmWindow);
+		mntmWindow.setMenu(menuWindow);
+		
+		MenuItem mntmListView = new MenuItem(menuWindow, SWT.RADIO);
+		mntmListView.setSelection(true);
+		mntmListView.setText(getResourceString("list"));
+		
+		MenuItem mntmCalendarView = new MenuItem(menuWindow, SWT.RADIO);
+		mntmCalendarView.setText(getResourceString("calendar"));
+	}
+
+	/**
+	 * @param menuBar
+	 */
+	private void createEditMenu(Menu menuBar) {
+		MenuItem mntmEdit = new MenuItem(menuBar, SWT.CASCADE);
+		mntmEdit.setText(getResourceString("menu.edit"));
+		
+		Menu menuEdit = new Menu(mntmEdit);
+		mntmEdit.setMenu(menuEdit);
+		
+		MenuItem mntmUndo = new MenuItem(menuEdit, SWT.NONE);
+		mntmUndo.setText(getResourceString("undo"));
+		
+		MenuItem mntmRedo = new MenuItem(menuEdit, SWT.NONE);
+		mntmRedo.setText(getResourceString("redo"));
+		
+		new MenuItem(menuEdit, SWT.SEPARATOR);
+		
+		MenuItem mntmSearch = new MenuItem(menuEdit, SWT.NONE);
+		mntmSearch.setText(getResourceString("find"));
+	}
+
+	/**
+	 * @param menuBar
+	 */
+	private void createUserMenu(Menu menuBar) {
+		MenuItem mntmUser = new MenuItem(menuBar, SWT.CASCADE);
+		mntmUser.setText(getResourceString("menu.user"));
+		
+		Menu menuUser = new Menu(mntmUser);
+		mntmUser.setMenu(menuUser);
+		
+		MenuItem mntmSave = new MenuItem(menuUser, SWT.NONE);
+		mntmSave.setText(getResourceString("save"));
+		
+		new MenuItem(menuUser, SWT.SEPARATOR);
+		
+		MenuItem mntmExit = new MenuItem(menuUser, SWT.NONE);
+		mntmExit.setText(getResourceString("exit"));
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean closeTaskMeter() {
+		if(isModified) {
+			//ask user if they want to save current address book
+			MessageBox box = new MessageBox(this, SWT.ICON_WARNING | SWT.YES | SWT.NO | SWT.CANCEL);
+			box.setText(this.getText());
+			box.setMessage(getResourceString("msg.close"));
+		
+			int choice = box.open();
+			if(choice == SWT.CANCEL) {
+				return false;
+			} else if(choice == SWT.YES) {
+				if (!save()) return false;
+			}
+		}
+			
+		return true;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean save() {
+		
+		return true;
+	}
+	
+	/**
+	 * 
+	 * @param tableItem
+	 */
+	private void editTask(TableItem tableItem) {
+		TaskDetailDialog dialog = new TaskDetailDialog(this);
+		/*
+		dialog.setLabels(columnNames);
+		String[] values = new String[table.getColumnCount()];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = item.getText(i);
+		}
+		dialog.setValues(values);
+		values = dialog.open();
+		if (values != null) {
+			item.setText(values);
+			isModified = true;
+		}
+		*/
+	}
+
+	private void setStatusBar(String msg) {
+		statusBar.setText(msg);
+	}
+
+	private void displayError(String msg) {
+		MessageBox box = new MessageBox(this, SWT.ICON_ERROR);
+		box.setMessage(msg);
+		box.open();
+	}
+	
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components

641ff40e20d2b6727f60aae9eb3901b4d654de98
SongYY <SongYY@SYY.nus.edu.sg>
2011-09-23 11:41:16 +0800

    Done the first draft for storage.

--- cs2103/t14j1/storage/FileHandler.java
+++ cs2103/t14j1/storage/FileHandler.java
@@ -1,6 +1,7 @@
 package cs2103.t14j1.storage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,6 +16,10 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import cs2103.t14j1.logic.DateFormat;
 
@@ -39,32 +44,93 @@ public class FileHandler {
 	private static final String taskFileName = "tasks.txt";
 	private static final String listFileName = "lists.txt";
 	
+	// add in magic string for the list tag
+	private static final String xmlListTag = "list";
+	private static final String xmlTaskTag = "task";
+	
+	// magic string for priority tag
+	private static final String xmlPriorityLow = "low";
+	private static final String xmlPriorityNormal = "normal";
+	private static final String xmlPriorityHigh = "high";
+	
+	// this should not appaer; create this just in case
+	private static final String xmlUndefined = "undefine";	
+	
 	private static final String LOAD_SUCCESS = "All Lists and Tasks are Ready!";
 	private static final String SAVE_SUCCESS = "All Lists and Tasks are Saved!";
 	
 	
 	
-	public static String loadAll(TaskLists lists) {
+	public static String loadAll(TaskLists lists) throws SAXException, IOException, ParserConfigurationException {
 		loadLists(lists);
 		loadTasks(lists);
 		
 		return LOAD_SUCCESS;
 	}
 	
-	private static void loadLists(TaskLists lists) {
-		// this is a demo of how to add a list
-		lists.add("test list");
+	private static boolean readXmlDocument(String folder, String fileName, Document document) throws SAXException, IOException, ParserConfigurationException{
+		File directory = new File(folder);
+		if(!directory.exists()){
+			directory.mkdir();
+		}
 		
-		// TODO: open the listFile and load it lists
+		File fXmlFile = new File(folder + fileName);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		document = dBuilder.parse(fXmlFile);
+		
+		return true;
+	}
+	
+	private static void loadLists(TaskLists lists) throws SAXException, IOException, ParserConfigurationException {
+		Document doc = null;
+		readXmlDocument(fileFolder,taskFileName,doc);
+		
+		NodeList listElements = doc.getElementsByTagName(xmlListTag);
+		for(int i = 0; i < listElements.getLength(); i++){
+			Node listElement = listElements.item(i);
+			
+			if(listElement.getNodeType() == Node.ELEMENT_NODE){
+				Element eListElement = (Element) listElement;
+				lists.add(eListElement.getNodeValue());
+			} else{
+				throw new SAXException("Wrong Element Type when parsing list file");
+			}
+		}
 	}
 
 	
-	private static void loadTasks(TaskLists lists) {
+	private static void loadTasks(TaskLists lists) throws SAXException, IOException, ParserConfigurationException {
+		Document doc = null;
+		readXmlDocument(fileFolder,taskFileName,doc);
 		
-		// these are demos of how to add a task into list
-		// delete these demos only after you finished the loading
-		// because they are used for other layer for testing
-		
+		NodeList taskElements = doc.getElementsByTagName(xmlTaskTag);
+		for(int i=0; i<taskElements.getLength(); i++){
+			// Node here is a task list
+			Node taskElement = taskElements.item(i);
+			NamedNodeMap taskAttr = taskElement.getAttributes();
+			String name = (taskAttr.getNamedItem(Task.NAME)).getNodeValue();
+			String list = (taskAttr.getNamedItem(Task.LIST)).getNodeValue();
+			Priority priority = translateStringToPriority((taskAttr.getNamedItem(Task.PRIORITY)).getNodeValue());
+			boolean status = Boolean.parseBoolean((taskAttr.getNamedItem(Task.STATUS)).getNodeValue());
+			
+			String startDateStr = (taskAttr.getNamedItem(Task.START_DATE)).getNodeValue();
+			Date startDate = null;
+			if(startDateStr.compareTo(xmlUndefined)!=0){
+				startDate = DateFormat.strToDateLong(startDateStr);
+			}
+			String endDateStr = (taskAttr.getNamedItem(Task.END_DATE)).getNodeValue();
+			Date endDate = null;
+			if(endDateStr.compareTo(xmlUndefined)!=0){
+				endDate = DateFormat.strToDateLong(endDateStr);
+			}
+			
+			// TODO: the time also need that kind of handling
+			Long start_time = Long.parseLong((taskAttr.getNamedItem(Task.START_TIME)).getNodeValue());
+			Long end_time = Long.parseLong((taskAttr.getNamedItem(Task.END_TIME)).getNodeValue());
+			
+			lists.addTask(list, new Task(name,list,priority,startDate,endDate,status));
+		}
 		// add one task
 		String name = "new task";
 		String list = "inbox";
@@ -89,6 +155,18 @@ public class FileHandler {
 		
 	}
 	
+	private static Priority translateStringToPriority(String priorityStr) throws IOException {
+		if(priorityStr.compareTo(xmlPriorityLow) == 0){
+			return Priority.LOW;
+		} else if(priorityStr.compareTo(xmlPriorityNormal) == 0){
+			return Priority.NORMAL;
+		} else if(priorityStr.compareTo(xmlPriorityHigh) == 0){
+			return Priority.IMPORTANT;
+		} else{
+			throw new IOException("Priority parsing problem when reading the Task xml File.");
+		}
+	}
+
 	public static String saveAll(TaskLists lists) throws ParserConfigurationException, TransformerException {
 		String[] listNames = lists.getListNames(); // it will give you all the lists name
 		
@@ -169,7 +247,7 @@ public class FileHandler {
 		
 		// root elements
 		Document doc = docBuilder.newDocument();
-		Element rootElement = doc.createElement("list");
+		Element rootElement = doc.createElement(xmlListTag);
 		doc.appendChild(rootElement);
 		
 		String listNames[] = lists.getListNames();
@@ -184,7 +262,7 @@ public class FileHandler {
 			for (int i = 0; i < numTasks; i++) {
 				Task task = list.getTask(i);
 				
-				Element taskElement = doc.createElement("task");
+				Element taskElement = doc.createElement(xmlTaskTag);
 				listElement.appendChild(taskElement);
 				
 				listElement.setAttribute(Task.NAME, task.getName());
@@ -195,6 +273,9 @@ public class FileHandler {
 				listElement.setAttribute(Task.START_TIME, task.getStartTime());
 				listElement.setAttribute(Task.END_TIME, task.getEndTime());
 				listElement.setAttribute(Task.STATUS, Boolean.toString(task.getStatus()));
+				// would implement this later cuz currently not defined in the task
+				// listElement.setAttribute(Task.PLACE, task.getList());
+				
 			}
 		}
 		
@@ -210,13 +291,13 @@ public class FileHandler {
 		String priorityStr = null;
 		switch (priority) {
 		case IMPORTANT:
-			priorityStr = "important";break;
+			priorityStr = xmlPriorityHigh;break;
 		case NORMAL:
-			priorityStr = "nurmal";	break;
+			priorityStr = xmlPriorityNormal;break;
 		case LOW:
-			priorityStr = "low";	break;
+			priorityStr = xmlPriorityLow;	break;
 		default:
-			priorityStr = "undefined";
+			priorityStr = xmlUndefined;
 		}
 		return priorityStr;
 	}

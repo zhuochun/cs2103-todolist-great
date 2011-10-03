1391c446d3317a5fe2cfc6f4697e0f7f6b2c01de
Shubham Goyal <shubham.goyal@live.in>
2011-10-04 05:23:45 +0800

    Added some functionalities to Control.java, the most imprtant being SEARCH. Had to spend a lot of time understanding the dofferences between Yangyu's and Zhuochun's different Date and Time representations. Also had to add support for a lot of unsupported parameters like duration, deadline etc. in Task.java

--- cs2103/t14j1/Logic/Control.java
+++ cs2103/t14j1/Logic/Control.java
@@ -2,6 +2,7 @@ package cs2103.t14j1.logic;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Map.Entry;
 
@@ -24,6 +25,7 @@ class Control {
 	
 	TaskLists lists;
 	ParseCommand parseCommand;
+	ArrayList<Task> latestSearchResults;
 
 	/**
 	 * command line interface, just for version 0.1
@@ -105,6 +107,8 @@ class Control {
 	/**
 	 * add task into TaskLists
 	 * 
+	 * Shubham: Have to add functionality for deadline
+	 * 
 	 * @param input
 	 * @return
 	 */
@@ -112,11 +116,20 @@ class Control {
 		String name        = parseCommand.extractTaskName();
 		String list        = parseCommand.extractListName();
 		Priority priority  = parseCommand.extractPriority();
+		String place 	   = parseCommand.extractPlace();
 		Date startDateTime = parseCommand.extractStartDate();
 		Date endDateTime   = parseCommand.extractEndDate();
-		boolean status    = Task.NOT_COMPLETED;
+		Long startTime     = parseCommand.extractStartTime();
+		Long endTime       = parseCommand.extractEndTime();
+		Date deadline 	   = parseCommand.extractDeadlineDate();
+		Long deadlineTime  = parseCommand.extractDeadlineTime();
+		Long duration      = parseCommand.extractDuration();
+		convertLongTimeToDateObject(startDateTime, startTime);
+		convertLongTimeToDateObject(endDateTime, endTime);
+		convertLongTimeToDateObject(deadline, deadlineTime);
+		boolean status     = Task.NOT_COMPLETED;
 		
-		Task newTask = new Task(name, list, priority, startDateTime, endDateTime, status);
+		Task newTask = new Task(name, list, priority, startDateTime, endDateTime, status, deadline, duration, place);
 		String result = lists.addTask(list, newTask);
 		
 		return result;
@@ -136,15 +149,116 @@ class Control {
 	//not yet complete
 	private String search(String input) {
 		
+		if(latestSearchResults.size() != 0)
+			latestSearchResults.clear();
+		
 		String name = parseCommand.extractTaskName();
-		Date startDate = parseCommand.extractStartDate();
-		Date endDate = parseCommand.extractEndDate();
+		Date startDateTime = parseCommand.extractStartDate();
+		Date endDateTime = parseCommand.extractEndDate();
 		Long startTime = parseCommand.extractStartTime();
 		Long endTime = parseCommand.extractEndTime();
 		String place = parseCommand.extractPlace();
 		Priority priority = parseCommand.extractPriority();
+		convertLongTimeToDateObject(startDateTime, startTime);
+		convertLongTimeToDateObject(endDateTime, endTime);
+		Date afterDate = parseCommand.extractSearchAfterDate();
+		Long afterTime = parseCommand.extractSearchAfterTime();
+		convertLongTimeToDateObject(afterDate, afterTime);
+		Date beforeDate = parseCommand.extractSearchBeforeDate();
+		Long beforeTime = parseCommand.extractSearchBeforeTime();
+		convertLongTimeToDateObject(beforeDate, beforeTime);
+		Date deadlineDate = parseCommand.extractDeadlineDate();
+		Long deadlineTime = parseCommand.extractDeadlineTime();
+		convertLongTimeToDateObject(deadlineDate, deadlineTime);
+		Long duration = parseCommand.extractDuration();
+		String listName = parseCommand.extractListName();
 		
-		return null;
+		Iterator<Entry<String, TaskList>> iterator = lists.iterator();
+		while(iterator.hasNext()) {
+			cs2103.t14j1.storage.TaskList taskList = iterator.next().getValue();
+			for(int i = 0; i < taskList.getSize(); i ++) {
+				if(isNameSame(name, taskList.getTask(i))
+						&& isStartDateSame(startDateTime, taskList.getTask(i))
+						&& isEndDateSame(endDateTime, taskList.getTask(i))
+						&& isDeadlineDateSame(deadlineDate, taskList.getTask(i))
+						&& isPlaceSame(place, taskList.getTask(i))
+						&& isPrioritySame(priority, taskList.getTask(i))
+						&& isDateAfterGivenDateAndTime(afterDate, taskList.getTask(i))
+						&& isDateBeforeGivenDateAndTime(beforeDate, taskList.getTask(i))
+						&& isDurationSame(duration, taskList.getTask(i))
+						&& isListNameSame(listName, taskList.getTask(i))) {
+					latestSearchResults.add(taskList.getTask(i));
+				}
+			}
+		}
+		
+		display(latestSearchResults);
+		
+		String SEARCH_RESULT;
+		
+		if(latestSearchResults.size() == 0)
+			SEARCH_RESULT = "No match found";
+		else
+			SEARCH_RESULT = "Search successful.";
+		
+		return SEARCH_RESULT;
+	}
+	
+	private boolean isListNameSame(String listName, Task task) {
+		return (listName.equals(task.getList()));
+	}
+
+	private boolean isDurationSame(Long duration, Task task) {
+		return (duration == task.getDuration());
+	}
+
+	private boolean isDateBeforeGivenDateAndTime(Date beforeDate, Task task) {
+		return (isDateBefore(task.getDeadline(), beforeDate) || isDateBefore(task.getEndDateInDateFormat(), beforeDate) || isDateBefore(task.getStartDateInDateFormat(), beforeDate));
+	}
+	
+	private boolean isDateBefore(Date a, Date b) {
+		return (a.getTime() <= b.getTime()); 
+	}
+
+	private boolean isDateAfterGivenDateAndTime(Date afterDate, Task task) {
+		return (isDateAfter(task.getDeadline(), afterDate) || isDateAfter(task.getEndDateInDateFormat(), afterDate) || isDateAfter(task.getStartDateInDateFormat(), afterDate));
+	}
+
+	private boolean isDateAfter(Date a, Date b) {
+		return (a.getTime() >= b.getTime());
+	}
+
+	private boolean isPrioritySame(Priority priority, Task task) {
+		return (priority == task.getPriority());
+	}
+
+	private boolean isPlaceSame(String place, Task task) {
+		return place.equals(task.getPlace());
+	}
+
+	private boolean isDeadlineDateSame(Date deadlineDate, Task task) {
+		return (deadlineDate.getTime() == task.getDeadline().getTime());
+	}
+
+	private boolean isEndDateSame(Date endDateTime, Task task) {
+		return (endDateTime.getTime() == task.getEndDateInDateFormat().getTime());
+	}
+
+	private boolean isStartDateSame(Date startDateTime, Task task) {
+		return (startDateTime.getTime() == task.getStartDateInDateFormat().getTime());
+	}
+
+	private boolean isNameSame(String name, Task task) {
+		return name.equals(task.getName());
+	}
+
+	private void convertLongTimeToDateObject (Date d, Long secondsFromStartOfDay) {
+		int hours     = secondsFromStartOfDay.intValue() / 3600;
+		int minutes   = (secondsFromStartOfDay.intValue() - hours * 60*60)/60;
+		int seconds   = secondsFromStartOfDay.intValue() - hours * 3600 - minutes * 60;
+		d.setHours(hours);
+		d.setMinutes(minutes);
+		d.setSeconds(seconds);
 	}
 
 	private String deleteTask(String input) {
@@ -195,6 +309,12 @@ class Control {
 			System.out.println((i + 1) + ". " + list.get(i).getName());
 		}
 		
+	}
+	
+	public void display(ArrayList<Task> a) {
+		for(int i = 0; i < a.size(); i ++) {
+			System.out.println((i + 1) + ". " + (a.get(i).getName()));
+		}
 	}
 	
 	public void switchList (String input) {

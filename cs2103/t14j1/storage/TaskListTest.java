cb388c698e190bdf054903d29fcb87ebb6a97d3b
SongYY <SongYY@SYY.nus.edu.sg>
2011-10-09 21:39:48 +0800

    Fix but for "on next Sat"

---
+++ cs2103/t14j1/storage/TaskListTest.java
@@ -0,0 +1,60 @@
+package cs2103.t14j1.storage;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskListTest {
+    
+    TaskList tlist;
+
+    @Before
+    public void setUp() throws Exception {
+        tlist = new TaskList("Test");
+        
+        String name = "new task";
+        String place = null;
+        String list = "Inbox";
+        Priority priority = Priority.IMPORTANT;
+        Date startDateTime = null;
+        Date endDateTime = null;
+        Date deadline = null;
+        Long duration = new Long(20);
+        boolean status = Task.INCOMPLETE;
+        
+        Task newTask = new Task(name, place, list, priority, startDateTime, endDateTime, deadline, duration, status);
+        
+        tlist.add(newTask);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetTask() {
+        Task task = tlist.getTask(1);
+        
+        System.out.println("\t" + task.getName());
+        System.out.println("\t" + task.getPriorityStr());
+        System.out.println("\t" + task.getStatusStr());
+        System.out.println("\t--------------");
+    }
+
+    @Test
+    public void testDelete() {
+        Task task = tlist.getTask(1);
+        
+        System.out.println("\t" + task.getName());
+        System.out.println("\t" + task.getPriorityStr());
+        System.out.println("\t" + task.getStatusStr());
+        System.out.println("\t--------------");
+        
+        tlist.delete(1);
+        
+        System.out.println(tlist.getSize());
+    }
+
+}
---
+++ cs2103/t14j1/storage/TaskLists.java
@@ -0,0 +1,171 @@
+package cs2103.t14j1.storage;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+/**
+ * Stores all the list, need this extra abstraction for GUI
+ * 
+ * @author Zhuochun
+ *
+ */
+public class TaskLists implements Iterable<Entry<String, TaskList>> {
+	
+	private TreeMap<String, TaskList> lists;
+	
+	/* Messages */
+	private static final String ADD_SUCCESS = "List \"%1$s\" is Successfully Added";
+	private static final String ADD_FAIL = "List \"%1$s\" already exists";
+	private static final String REMOVE_SUCCESS = "List \"%1$s\" is Successfully Removed";
+	private static final String MOVE_SUCCESS = "Task \"%1$s\" is Successfully Moved from List \"%2$s\" to List \"%3$s\"";
+	private static final String MOVE_FAIL = "Task is not found in List \"%1$s\"";
+
+    /* Default Lists */
+	public static final String INBOX = "Inbox";
+	public static final String TRASH = "Trash";
+	
+	/* constructor */
+	public TaskLists() {
+		lists = new TreeMap<String, TaskList>();
+		// add default lists
+		add(INBOX);
+		add(TRASH);
+	}
+	
+	/**
+	 * add a new list
+	 * 
+	 * @param name 					new list name
+	 * @return
+	 * 	// added by Songyy
+	 *  The result of adding a list
+	 */
+	public String add(String name) {
+	    if (lists.containsKey(name)) {
+	        return String.format(ADD_FAIL, name);
+	    }
+	    
+	    return add(new TaskList(name));
+	}
+
+	/**
+	 * add an existing TaskList
+	 * 
+	 * @param list
+	 * @return
+	 * 	// added by Songyy
+	 *  The result of adding a list
+	 */
+	public String add(TaskList list) {
+	    lists.put(list.getName(), list);
+	    
+		return String.format(ADD_SUCCESS, list.getName());
+	}
+	
+	/**
+	 * delete a existing list
+	 * 
+	 * @param name
+	 * @return
+	 * 	result of return
+	 */
+	public String remove(String name) {
+		TaskList list = lists.remove(name);
+		
+		return String.format(REMOVE_SUCCESS, list.getName());
+	}
+	
+	/**
+	 * add a task into list
+	 * 
+	 * @param listName			the list name of the task will be added to
+	 * @param task				the task object
+	 * @return
+	 * 	// added by Songyy
+	 *  The result of adding a list
+	 */
+	public String addTask(String listName, Task task) {
+		String result;
+		
+		if (lists.containsKey(listName)) {
+			result = lists.get(listName).add(task);
+		} else { // if the list does not exist, create it
+			add(listName); 
+			result = lists.get(listName).add(task);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * remove a task from a list
+	 * 
+	 * @param listName         the list which the task stays
+	 * @param index            index of the task in list
+	 * @return
+	 * // added by Songyy
+	 *  The result of adding a list
+	 */
+	public String deleteTask(String listName, int index) {
+	    return lists.get(listName).delete(index);
+	}
+	
+	/**
+	 * move a task from old list to new list
+	 * 
+	 * @param oldList          the old list name
+	 * @param newList          the new list name
+	 * @param index            index of the task in old list
+	 * @return
+	 * 	the result shown to the user
+	 */
+	public String moveTask(String oldList, String newList, int index) {
+	    TaskList oldlist = getList(oldList);
+	    
+	    Task task = oldlist.getTask(index);
+	    
+	    if (task != null) {
+	        task.setList(newList);
+	        
+	        addTask(newList, task);
+	        
+	        oldlist.delete(index);
+	    } else {
+	        return String.format(MOVE_FAIL, oldList);
+	    }
+	    
+	    return String.format(MOVE_SUCCESS, task.getName(), oldList, newList);
+	}
+	
+	/**
+	 * get the specific list with name passed
+	 * 
+	 * @param name
+	 * @return
+	 *  the result shown to the user
+	 */
+	public TaskList getList(String name) {
+		return lists.get(name);
+	}
+	
+	/**
+	 * check whether the list name already exists
+	 * 
+	 * @param name
+	 * @return
+	 *  the result shown to the user
+	 */
+	public boolean hasList(String name) {
+	    return lists.containsKey(name);
+	}
+	
+	/**
+	 * iteration of lists in TaskLists
+	 */
+    public Iterator<Entry<String, TaskList>> iterator() {
+        Iterator<Entry<String, TaskList>> iterateList = lists.entrySet().iterator();
+        return iterateList;
+    }
+
+}

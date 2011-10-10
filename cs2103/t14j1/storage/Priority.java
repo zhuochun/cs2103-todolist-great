2f45c1a0b9c3be757c4a9718c04d89d83ef75b7a
Wang Zhuochun <stone1551@gmail.com>
2011-10-10 15:26:53 +0800

    Refractor Storage and GUI parts

--- cs2103/t14j1/storage/Priority.java
+++ cs2103/t14j1/storage/Priority.java
@@ -1,23 +1,7 @@
 package cs2103.t14j1.storage;
 
-/**
- * @author Zhuochun
- * @author SongYY
- * 	for adding in the comments
- */
 public enum Priority {
-	/**
-	 * important task -- valued 1
-	 */
 	IMPORTANT,
-	
-	/**
-	 * Normal Task -- valued 2
-	 */
 	NORMAL,
-	
-	/**
-	 * Low priority task -- valued 3
-	 */
 	LOW,
 }

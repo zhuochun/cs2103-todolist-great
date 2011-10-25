9ead9f8e066dd48cccad360fa7f89dfe41eb4913
Wang Zhuochun <stone1551@gmail.com>
2011-10-26 02:01:55 +0800

    better display for search result

---
+++ cs2103/t14j1/Logic/Commands.java
@@ -0,0 +1,74 @@
+package cs2103.t14j1.logic;
+
+public enum Commands {
+
+    /**
+     * Syntax: add + task str
+     */
+    ADD_TASK,
+
+    /**
+     * Syntax: del + [num]
+     */
+    DELETE_TASK,
+
+    /**
+     * Syntax: (move|mv) [num] [#list]
+     */
+    MOVE_TASK,
+    
+    /**
+     * Syntax: edit + [num]
+     */
+    EDIT_TASK,
+    
+    /**
+     * Syntax 1: [num] + done
+     * Syntax 2: done + [num]
+     */
+    MARK_COMPLETE,
+    
+    /**
+     * Syntax: [num] + [!1~3]
+     */
+    MARK_PRIORITY,
+
+    /**
+     * Syntax: add [#list]
+     */
+    ADD_LIST,
+
+    /**
+     * Syntax: rename [#list] [#list]
+     */
+    EDIT_LIST,
+
+    /**
+     * Syntax: del + [#list]
+     */
+    DELETE_LIST,
+
+    /**
+     * Syntax: #(list name)
+     */
+    SWITCH_LIST,
+
+    /**
+     * Syntax: / + string to search
+     */
+    SEARCH,
+
+    /**
+     * Syntax: dis/display + [#list]
+     */
+    DISPLAY_LISTS,
+
+    /**
+     * Syntax: dis/display + [num]
+     */
+    DISPLAY_TASK,
+
+    EXIT,
+
+    INVALID
+}

b12c107089af447c9b7ccdb87c346916e8a5e315
Wang Zhuochun <stone1551@gmail.com>
2011-10-31 16:10:08 +0800

    finish addList, addTask, deleteTask Event

--- cs2103/t14j1/logic/events/AddTask.java
+++ cs2103/t14j1/logic/events/AddTask.java
@@ -19,17 +19,14 @@ public class AddTask extends Event {
         if (success) {
             feedback = String.format(eventHandler.getMsg("msg.ADD_TASK_SUCCESS"), task.getName(), task.getList());
             
+            eventHandler.refreshLists();
+            eventHandler.switchTask(task.getList());
+            
             eventHandler.setModified();
-
-            if (mode == MODE_LIST && newTask.getList().equals(currentList.getName())) {
-                displayNewTask(taskTable.getItemCount()+1, newTask);
-            }
         } else {
             feedback = String.format(eventHandler.getMsg("msg.ADD_FAIL"), "TASK");
         }
 
-        eventHandler.refreshLists();
-        eventHandler.switchTask(task.getList());
         eventHandler.setStatus(feedback);
     }
     

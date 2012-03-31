b6f0f3940adb0c9c097bff86e859bc16a83abf1b
songyy <flyfy1@gmail.com>
2012-04-01 02:28:39 +0800

    Make the 'quit' button to minimize the task browser, instead of quit.

---
+++ gui/Main.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import org.eclipse.swt.widgets.Display;
+
+import storage.logging.Log;
+import storage.user.User;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(TaskMeter.class.getName());
+
+    /**
+     * Launch the application.
+     * 
+     * @param args
+     */
+    public static void main(String args[]) {
+        try {
+            Log.setup();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        User.initial();
+        
+        final Display display = Display.getDefault();
+        try {
+            TaskMeter application = new TaskMeter(display);
+            application.open();
+            application.layout();
+            while (!application.isDisposed()) {
+                if (!display.readAndDispatch()) {
+                    display.sleep();
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "main", e);
+        }
+    }
+}
